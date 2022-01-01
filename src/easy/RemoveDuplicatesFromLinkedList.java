e5116b94e6e685af818382a708cfb6f005b0aaa7
ykucukcinar <yigitkucukcinar@hotmail.com>
2022-01-01 18:28:03 +0300

    Remove Duplicates From Linked List solution added.

---
+++ src/easy/RemoveDuplicatesFromLinkedList.java
@@ -0,0 +1,29 @@
+package easy;
+
+public class RemoveDuplicatesFromLinkedList {
+
+    //Remove duplicates in a Linked List
+
+    public static class LinkedList {
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+        LinkedList currentNode = linkedList;
+        while (currentNode != null){
+            LinkedList nextDistinctNode = currentNode.next;
+            while (nextDistinctNode != null && nextDistinctNode.value == currentNode.value){
+                nextDistinctNode = nextDistinctNode.next;
+            }
+            currentNode.next = nextDistinctNode;
+            currentNode = nextDistinctNode;
+        }
+        return linkedList;
+    }
+}
